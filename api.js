5719f56511b82beec2993441b73247576c2fa652
cobalt88 <vincentteune@gmail.com>
2022-04-30 05:33:21 -0400

    4.30.22
    initial setup

---
+++ api.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const PORT = 3005;
+const path = require('path');
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('data'))
+
+app.get('/', (req, res) => {
+  res.send("hello world")
+})
+
+app.get('/recipes', (req, res) => {
+  let options = {
+    root: path.join(__dirname, 'data'),
+  }
+  res.sendFile('test.json', options)
+})
+
+app.get('/names', (req, res) => {
+  let options = {
+    root: path.join(__dirname, 'data')
+  }
+  res.sendFile('names.json', options)
+})
+
+app.post('/names', (req, res) => {
+  console.log(req.body);
+  res.send(`request received ${req}`)
+})
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
+
+//   ./data/names.json
